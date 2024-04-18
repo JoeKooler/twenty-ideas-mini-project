76fa779cdb9c46d4a992af26bc3ea62f8be5e277
joekooler <jojesin@gmail.com>
2024-04-18 18:22:20 +0700

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import HomePage from './pages/home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+]);
 
 function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <HomePage />
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </>
   );
